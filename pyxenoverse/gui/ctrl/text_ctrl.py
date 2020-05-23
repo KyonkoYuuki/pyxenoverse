83d70f85912c09b9b018cd72a17edad560a77b41
KyonkoYuuki <kyonkoyuuki@users.noreply.github.com>
2020-05-23 02:52:16 -0400

    Add maxlen argument for TextCtrl

--- pyxenoverse/gui/ctrl/text_ctrl.py
+++ pyxenoverse/gui/ctrl/text_ctrl.py
@@ -3,7 +3,12 @@ import wx
 
 class TextCtrl(wx.TextCtrl):
     def __init__(self, *args, **kwargs):
+        maxlen = 0
+        if "maxlen" in kwargs:
+            maxlen = kwargs.pop("maxlen")
         wx.TextCtrl.__init__(self, *args, **kwargs)
+        if maxlen:
+            self.SetMaxLength(maxlen)
 
     def GetValue(self):
         return super().GetValue()
