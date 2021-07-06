6c4c7e6211a984b2041c2e43d0cd86f999de95fc
Unleashed <shadowmahdi1998@gmail.com>
2021-07-07 01:08:27 +0300

    size fix - redid order type0

--- pyxenoverse/bdm/subentry/type0.py
+++ pyxenoverse/bdm/subentry/type0.py
@@ -4,34 +4,50 @@ from pyxenoverse.bdm.subentry import BaseType
 
 BDMType0 = recordclass('BDMType0', [
     'damage_type',
-    'secondary_type',
-    'damage_amount',
-    'damage_special',
-    'sound_type',
+    'u_02',
+    'damage_amount', #good
+    'u_06',
+    'f_08',
+    'sound_type', #good1
     'sound_id',
     'effect_1_eepk_id',
     'effect_1_skill_id',
     'effect_1_skill_type',
+    'u_16',
     'effect_2_eepk_id',
     'effect_2_skill_id',
     'effect_2_skill_type',
+    'u_1e',
     'effect_3_eepk_id',
     'effect_3_skill_id',
     'effect_3_skill_type',
+    'u_26',
     'pushback_speed',
     'pushback_acceleration_percent',
     'user_stun',
     'victim_stun',
     'knockback_duration',
-    'knockback_ground_impact_time',
     'knockback_recovery_after_impact_time',
-    'knockback_gravity_time',
+    'knockback_ground_impact_time',
+    'u_3a',
     'knockback_strength_x',
     'knockback_strength_y',
     'knockback_strength_z',
     'knockback_drag_y',
+    'u_4c',
+    'knockback_gravity_time',
     'victim_invincibility_time',
+    'u_52',
+    'transformation_type',
     'ailment_type',
+    'u_58_1', #array
+    'u_58_2',
+    'u_58_3',
+    'damage_special',
+    'u_60_1', #array
+    'u_60_2',
+    'stumble_type',
+    'secondary_type',
     'camera_shake_type',
     'camera_shake_time',
     'user_screen_flash_transparency',
@@ -41,29 +57,13 @@ BDMType0 = recordclass('BDMType0', [
     'user_animation_time',
     'victim_animation_time',
     'user_animation_speed',
-    'victim_animation_speed',
-    'transformation_type',
-    'stumble_type',
-    'u_02',
-    'u_06',
-    'f_08',
-    'u_22',
-    'u_30',
-    'u_38',
-    'u_58',
-    'u_76',
-    'u_82',
-    'u_88_1', #array
-    'u_88_2',
-    'u_88_3',
-    'u_96_1',#array
-    'u_96_2'
+    'victim_animation_speed'
 ])
 
 
 class Type0(BaseType):
     bac_record = BDMType0
-    byte_order = 'HHHHfHhhhHHhhHHhhHHffHHHHHHffffHHhHHhHHHHHHHHHHhhhHHHfI'
+    byte_order = 'HHHHfHhhhHHhhHHhhHHffHHHHHHffffHHhHHhHHHHHHHHHHhhhHHHff'
     size = 128
 
     def convert_type1_to_type0(self, type1):
